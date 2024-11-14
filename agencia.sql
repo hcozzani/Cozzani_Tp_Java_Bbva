868eca64b7fa1d947129b007fbf574594502b987
Hugo Cozzani <113481573+hcozzani@users.noreply.github.com>
2024-11-13 21:00:21 -0300

    se agrego endpoint

---
+++ agencia.sql
@@ -0,0 +1,17 @@
+create database agencia;
+
+drop database agencia;
+
+INSERT INTO clientes VALUES
+(4,'Pedro Varela 111','12345444', 'horacio.benetti@gmail.com', 'Horacio', '1112300321');
+
+INSERT INTO tipos_seguros  VALUES
+(4, 'Cobertura completa para celulares', 'activo','Seguro de celular','1000');
+
+insert into polizas values 
+(4, 'alta','2021-11-04','2025-02-10',30000,4,4);
+
+select * from polizas
+usuarios;
+
+INSERT INTO USUARIOS VALUES (1, 1, '123', 'ADMIN', 'hugo');
